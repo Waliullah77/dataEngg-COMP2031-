2299ca0e8df4d99b91c0bc5ceec33085b00e06d8
Mohammad Wali Ullah <80650320+Waliullah77@users.noreply.github.com>
2022-04-12 10:22:37 +0930

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+
+# Variables that contains the user credentials to access Twitter API
+
+ACCESS_TOKEN = "1501454664646103040-0fcPOISZ6jZLukSyPWctNcHxgo5dMq"
+ACCESS_TOKEN_SECRET = "9rgsGxUr5MFsueUo2ZCFBaGYJwqyB84h3cgktRsCjn4wT"
+CONSUMER_KEY = "WsyPBj2rNEVnG9o6Aizn43thO"
+CONSUMER_SECRET = "iwqjdePjAvK1Q4h9baAsduVtHXwhvGT9QlPcZlj9X9P31RfRSy"
